63b20a3546db42be91d34d669f6a039c00f951e8
Alessandro Scoccia Pappagallo <alessandrosp@alessandrosp-macbookpro2.roam.corp.google.com>
2018-09-01 16:05:09 +0100

    Moved Client into dedicated module

---
+++ gspread_db/client.py
@@ -0,0 +1,27 @@
+import gspread
+
+from gspread_db import Database
+
+
+class Client(gspread.Client):
+
+    def open(self, title):
+        """Opens a spreadsheet."""
+        spreadsheet = super(Client, self).open(title)
+        return Database(spreadsheet.client, spreadsheet._properties)
+
+    def open_by_key(self, key):
+        """Opens a spreadsheet specified by key."""
+        spreadsheet = super(Client, self).open_by_key(key)
+        return Database(spreadsheet.client, spreadsheet._properties)
+
+    def open_by_url(self, url):
+        """Opens a spreadsheet specified by url."""
+        spreadsheet = super(Client, self).open_by_url(url)
+        return Database(spreadsheet.client, spreadsheet._properties)
+
+    def openall(self, title=None):
+        """Opens all available spreadsheets."""
+        spreadsheets = super(Client, self).openall(title)
+        return [Database(spreadsheet.client, spreadsheet._properties)
+                for spreadsheet in spreadsheets]
--- gspread_db/gspread_db.py
+++ gspread_db/gspread_db.py
@@ -36,30 +36,6 @@ def authorize(credentials, client_class=gspread.client.Client):
     return Client(client.auth, client.session)
 
 
-class Client(gspread.Client):
-
-    def open(self, title):
-        """Opens a spreadsheet."""
-        spreadsheet = super(Client, self).open(title)
-        return Database(spreadsheet.client, spreadsheet._properties)
-
-    def open_by_key(self, key):
-        """Opens a spreadsheet specified by key."""
-        spreadsheet = super(Client, self).open_by_key(key)
-        return Database(spreadsheet.client, spreadsheet._properties)
-
-    def open_by_url(self, url):
-        """Opens a spreadsheet specified by url."""
-        spreadsheet = super(Client, self).open_by_url(url)
-        return Database(spreadsheet.client, spreadsheet._properties)
-
-    def openall(self, title=None):
-        """Opens all available spreadsheets."""
-        spreadsheets = super(Client, self).openall(title)
-        return [Database(spreadsheet.client, spreadsheet._properties)
-                for spreadsheet in spreadsheets]
-
-
 class Database(gspread.models.Spreadsheet):
 
     def __getitem__(self, table_name):
